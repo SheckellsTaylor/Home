c73a6a717a7ac6aa4af6c7a826c2a2a97e532513
SheckellsTaylor <taylorlentzsheckells@gmail.com>
2018-08-06 19:30:06 -0700

    adding authentication and styles from prebuilt layout. Layout will be adjusted to fit my needs. It currently loads slow..So i am not sure how much i like it.. Next step is to add dashboard page after a user logs in which will be just myself when I upload a blog post. After that then I will add content.

--- Portfolio/app/Http/Controllers/Auth/AuthController.php
+++ Portfolio/app/Http/Controllers/Auth/AuthController.php
@@ -62,4 +62,9 @@ class AuthController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+protected $redirectPath = '/dashboard';
+protected $loginPath = '/login';
+
+
+
 }
